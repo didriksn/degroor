8548eff003851de49cda0c3b6c14ff71a7cc6fb0
didriksn <didrik.sommersten@gmail.com>
2022-11-04 15:47:38 +0100

    Fixed dark mode sidebar and made header dark mode (not done)

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 let toggle = true;
 const drk = document.getElementById("dark-mode");
 const videotitle = document.getElementsByClassName("video-title");
+const sidebartext = document.getElementsByClassName("sidebar-text");
  
 drk.addEventListener("click", function onClick() {
-  if (toggle == true) {
+  if (toggle == true) { // DARK MODE
     document.body.style.backgroundColor = "rgb(16,16,16)"; 
     Array.from(videotitle).forEach(el => {
       el.style.color = "white";
     });
+    Array.from(sidebartext).forEach(el => {
+      el.style.color = "rgb(200,200,200)";
+    });
     document.getElementById("dark-mode-text").innerHTML = "Light Mode"; 
     document.getElementById("sidebar").style.backgroundColor = "rgb(24,24,24)";
+    document.getElementById("header").style.backgroundColor = "rgb(40,40,40)";
+    document.getElementById("search-button").className = "search-button-dark";
+    document.getElementById("search-bar").className = "search-bar-dark";
     toggle = false; 
   } 
   
-  else {
+  else { // LIGHT MODE
     document.body.style.backgroundColor = "rgb(239,239,239)"; 
     Array.from(videotitle).forEach(el => {
       el.style.color = "black";
     }); 
+    Array.from(sidebartext).forEach(el => {
+      el.style.color = "black";
+    });
     document.getElementById("dark-mode-text").innerHTML = "Dark Mode";
     document.getElementById("sidebar").style.backgroundColor = "rgb(231,231,231)";
+    document.getElementById("header").style.backgroundColor = "white";
+    document.getElementById("search-button").className = "search-button";
+    document.getElementById("search-bar").className = "search-bar";
     toggle = true; 
   };
 });
